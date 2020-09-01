59354d292e4372ada703cf351bba1f8cfc78e293
Tim Wundenberg <timwundenberg@outlook.de>
2020-09-01 17:45:52 +0200

    #1 added classes

---
+++ CarpoolTracker/CarpoolTracker/Models/Drive.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarpoolTracker.Models
+{
+    public class Drive
+    {
+        public string Id { get; set; }
+        public Track Track { get; set; }
+        public List<Person> People { get; set; }
+    }
+}
