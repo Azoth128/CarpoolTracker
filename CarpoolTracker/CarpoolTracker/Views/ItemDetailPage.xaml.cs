61435c9d0effb81ce3154bbdeb1edf94cfbdbf0e
Tim Wundenberg <timwundenberg@outlook.de>
2020-09-01 17:16:37 +0200

    Projektdateien hinzufügen.

---
+++ CarpoolTracker/CarpoolTracker/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using CarpoolTracker.ViewModels;
+
+namespace CarpoolTracker.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
